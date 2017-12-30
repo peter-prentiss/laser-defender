c99dac3711bcac345a4cef2657f7fadc63ac2bd5
Peter <peter.prentiss@gmail.com>
2017-12-29 23:19:07 -0600

    Enemies respawn on delay.

--- Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -6,6 +6,7 @@ public class EnemySpawner : MonoBehaviour {
 	public float width = 10f;
 	public float height = 5f;
 	public float speed = 5;
+	public float spawnDelay = 0.5f;
 
 	private bool movingRight = true;
 	private float xmax;
@@ -17,7 +18,7 @@ public class EnemySpawner : MonoBehaviour {
 		Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1,0, distanceToCamera));
 		xmax = rightBoundary.x;
 		xmin = leftBoundary.x;
-		SpawnEnemies();
+		SpawnUntilFull();
 	}
 
 	public void OnDrawGizmos() {
@@ -40,8 +41,17 @@ public class EnemySpawner : MonoBehaviour {
 		}
 
 		if (AllMembersDead()) {
-			SpawnEnemies();
+			SpawnUntilFull();
 		}
+	}
+
+	Transform NextFreePosition() {
+		foreach(Transform childPositionGameObject in transform) {
+			 if (childPositionGameObject.childCount == 0){
+				 return childPositionGameObject ;
+			 }
+		}
+		return null; 
 	}
 
 	bool AllMembersDead() {
@@ -57,6 +67,17 @@ public class EnemySpawner : MonoBehaviour {
 		foreach( Transform child in transform) {
 			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
+		}
+	}
+
+	void SpawnUntilFull() {
+		Transform freePosition = NextFreePosition();
+		if (freePosition) {
+			GameObject enemy = Instantiate(enemyPrefab, freePosition.position, Quaternion.identity) as GameObject;
+			enemy.transform.parent = freePosition;
+		}
+		if (NextFreePosition()) {
+			Invoke ("SpawnUntilFull", spawnDelay);
 		}
 	}
 }
