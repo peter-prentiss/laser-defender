84a7fcc13c649d833a2762fea1795ca96e4ea781
Peter <peter.prentiss@gmail.com>
2018-01-11 12:50:20 -0600

    Add music.

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 	static MusicPlayer instance = null;
+
+	public AudioClip startClip;
+	public AudioClip gameClip;
+	public AudioClip endClip;
+
+	private AudioSource music;
 	
 	void Start () {
 		if (instance != null && instance != this) {
@@ -11,7 +17,28 @@ public class MusicPlayer : MonoBehaviour {
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
+			music = GetComponent<AudioSource>();
+			music.clip = startClip;
+			music.loop = true;
+			music.Play();
 		}
 		
+	}
+
+	void OnLevelWasLoaded(int level) {
+		Debug.Log("Music player loaded" + level); 
+		music.Stop();
+
+		if (level == 0) {
+			music.clip = startClip;
+		}
+		if (level == 1) {
+			music.clip = gameClip;
+		}
+		if (level == 2) {
+			music.clip = endClip;
+		}
+		music.loop = true;
+		music.Play();
 	}
 }
